5d41b3c7eb45dab4bad48c9c7f47f7bd3df8da2a
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-10-31 21:05:15 +0600

    create page and route

---
+++ src/Pages/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>this is blog page</h1>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/Pages/Shop/Shop.jsx
@@ -0,0 +1,11 @@
+
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>this is shop page</h1>
+    </div>
+  );
+};
+
+export default Shop;
