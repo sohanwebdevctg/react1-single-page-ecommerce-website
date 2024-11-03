bd68bbdae8e6281d8485a671bc3221ca3da0b9eb
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-11-03 15:12:24 +0600

    responsive the cart

--- src/Components/HomeCom/Cart.jsx
+++ src/Components/HomeCom/Cart.jsx
@@ -14,7 +14,7 @@ const Cart = ({item}) => {
             <div>
             {/* image section start */}
             <div className="bg-white">
-              <img src={item.image} className='w-full h-48'></img>
+              <img src={item.image} className='w-full h-28 sm:h-28 md:h-28 lg:h-36 xl:h-48 2xl:h-52'></img>
             </div>
             {/* image section end */}
             {/* description section start */}
@@ -23,22 +23,22 @@ const Cart = ({item}) => {
             <div className=" bg-[#F5F5F5] p-2 rounded-b-2xl shadow-xl">
               <ul className='space-y-1'>
                 <li className="flex justify-between items-center">
-                  <h2 className="text-sm md:text-xs xl:tex-sm font-bold italic">{item?.title}</h2>
-                  <p className="text-sm md:text-xs xl:text-sm font-bold">$ 40</p>
+                  <h2 className="text-[10px] sm:text-[11px] md:text-[11px] lg:text-xs xl:tex-sm 2xl:text-base font-bold italic">{item?.title}</h2>
+                  <p className="text-[10px] sm:text-[11px] md:text-[11px] lg:text-xs xl:text-sm 2xl:text-base font-bold">$ 40</p>
                 </li>
-                <li><p className="text-xs md:text-[10px] xl:text-xs text-gray-500">{item?.details.slice(0,100)}...</p></li>
+                <li><p className="text-[11px] sm:text-[11px] md:text-[10px] lg:text-[10px] xl:text-xs 2xl:text-sm text-gray-500">{item?.details.slice(0,100)}...</p></li>
                 <li className="flex justify-between items-center">
                   <span className="flex items-center">
-                    <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-xs"></FaStar>
-                    <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-xs"></FaStar>
-                    <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-xs"></FaStar>
-                    <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-xs"></FaStar>
-                    <FaStar className="text-yellow-500 text-xs md:text-[9px] xl:text-xs"></FaStar>
-                    <span className="text-xs md:text-[9px] xl:text-xs text-black ml-1">{item.rating}</span>
+                    <FaStar className="text-yellow-500 text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm"></FaStar>
+                    <FaStar className="text-yellow-500 text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm"></FaStar>
+                    <FaStar className="text-yellow-500 text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm"></FaStar>
+                    <FaStar className="text-yellow-500 text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm"></FaStar>
+                    <FaStar className="text-yellow-500 text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm"></FaStar>
+                    <span className="text-[10px] sm:text-[11px] md:text-[9px] lg:text-[10px] xl:text-xs 2xl:text-sm text-black ml-1">{item.rating}</span>
                   </span>
                   <span className="flex items-center gap-1">
-                    <AiFillPlusCircle className="text-red-500 text-xl md:text-sm xl:text-xl"></AiFillPlusCircle>
-                    <FaHeart className="text-red-500 text-lg md:text-sm xl:text-xl"></FaHeart>
+                    <AiFillPlusCircle className="text-red-500 text-base sm:text-base md:text-sm lg:text-sm xl:text-xl 2xl:text-2xl"></AiFillPlusCircle>
+                    <FaHeart className="text-red-500 text-sm sm:text-sm md:text-xs lg:text-xs xl:text-xl 2xl:text-xl"></FaHeart>
                   </span>
                 </li>
               </ul>
--- src/Components/HomeCom/NewProducts.jsx
+++ src/Components/HomeCom/NewProducts.jsx
@@ -24,14 +24,14 @@ const NewProducts = () => {
     <>
       <div className="my-8 xl:my-10">
         {/* content section start */}
-        <div className="container mx-auto xl:px-10">
+        <div className="container mx-auto px-3 sm:px-4 md:px-5 lg:px-7 xl:px-10">
           {/* title start */}
           <div className="mb-5 text-center">
-            <h2 className="text-2xl sm:text-2xl md:text-xl lg:text-3xl xl:text-4xl font-medium">Our Gallery</h2>
+            <h2 className="text-2xl sm:text-2xl md:text-xl lg:text-3xl xl:text-4xl 2xl:text-4xl font-medium">New Product</h2>
           </div>
           {/* title end */}
           {/* cart start */}
-          <div className="grid grid-cols-5">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5 gap-1 sm:gap-2 md:gap-1 lg:gap-2 xl:gap-3 2xl:gap-4">
             {
               datas.slice(0,10).map((item, id) => <Cart
               key={id}
